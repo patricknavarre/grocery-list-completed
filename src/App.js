e60fb749f167a5f157b90352aaa5624eada8ec5e
Patrick <patnavarre@gmail.com>
2021-04-18 21:13:29 -0400

    context added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,38 @@ import React , { useEffect, useState } from 'react';
 import "./App.css"
 
 import GroceryInput from "./GroceryInput";
+import Grocery from "./Grocery"
+
+let groceryObj = [
+  {
+    id: 1, 
+    grocery: "lobster",
+    isGrabbed: false,
+  },
+  {
+    id: 2,
+    grocery: "steak",
+    isGrabbed: false,
+  }, 
+  {
+    id: 3,
+    grocery: "cheese",
+    isGrabbed: false,
+  }
+]
 
 function App() {
 
+  const [groceryArray, setGroceryArray] = useState(groceryObj)
+
   function showGroceryInput() {
     return <GroceryInput />;
+  }
+
+  function showGrocery() {
+    return groceryArray.map((item) => {
+      return <Grocery key={item.id} />
+    })
   }
 
 
@@ -14,6 +41,7 @@ function App() {
   return (
     <div className="App">
     {showGroceryInput()}
+    {showGrocery()}
     </div>
   );
 }
